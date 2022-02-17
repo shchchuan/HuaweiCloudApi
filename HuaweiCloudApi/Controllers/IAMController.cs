7ebd56e7595933ef8e4a9ff325708bf55427c8bc
shchchuan <47128623+shchchuan@users.noreply.github.com>
2022-02-17 09:00:27 +0800

    initial

---
+++ HuaweiCloudApi/Controllers/IAMController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using HuaweiCloud.SDK.Core;
+using HuaweiCloud.SDK.Core.Auth;
+using HuaweiCloud.SDK.Iam;
+using HuaweiCloud.SDK.Iam.V3;
+using HuaweiCloud.SDK.Iam.V3.Model;
+using HuaweiCloudApi.Models;
+
+namespace HuaweiCloudApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IAMController : Controller
+    {
+        public IAMController()
+        {
+
+        }
+        /// <summary>
+        /// get token
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public MobileBaseResponseData<CreateTemporaryAccessKeyByTokenResponse> GetToken()
+        {
+            var res = new MobileBaseResponseData<CreateTemporaryAccessKeyByTokenResponse>();
+            const string ak = "XP4H5VOL8STTN6PKUMRP";
+            const string sk = "V2oO5T03aFtCVSZlvD8hfSpTGd5PCAIN9wIAv9OU";
+            var config = HttpConfig.GetDefaultConfig();
+            config.IgnoreSslVerification = true;
+            var auth = new GlobalCredentials(ak, sk);
+
+            var client = IamClient.NewBuilder()
+                    .WithCredential(auth)
+                    .WithRegion(IamRegion.ValueOf("cn-east-2"))
+                    .WithHttpConfig(config)
+                    .Build();
+            var req = new CreateTemporaryAccessKeyByTokenRequest
+            {
+            };
+            List<TokenAuthIdentity.MethodsEnum> listTokenAuthIdentityMethods = new List<TokenAuthIdentity.MethodsEnum>();
+            listTokenAuthIdentityMethods.Add(TokenAuthIdentity.MethodsEnum.FromValue("token"));
+            TokenAuthIdentity identityAuth = new TokenAuthIdentity()
+            {
+                Methods = listTokenAuthIdentityMethods
+            };
+            TokenAuth authbody = new TokenAuth()
+            {
+                Identity = identityAuth
+            };
+            req.Body = new CreateTemporaryAccessKeyByTokenRequestBody()
+            {
+                Auth = authbody
+            };
+            CreateTemporaryAccessKeyByTokenResponse resp = new CreateTemporaryAccessKeyByTokenResponse();
+            try
+            {
+                resp = client.CreateTemporaryAccessKeyByToken(req);
+                res.Status = EReponseCode.Success;
+                res.Message = "操作成功";
+                res.Data = resp;
+            }
+            catch (RequestTimeoutException requestTimeoutException)
+            {
+                Console.WriteLine(requestTimeoutException.ErrorMessage);
+                res.Status = EReponseCode.BusinessError;
+                res.Message = requestTimeoutException.ErrorMessage;
+            }
+            catch (ServiceResponseException clientRequestException)
+            {
+                Console.WriteLine(clientRequestException.HttpStatusCode);
+                Console.WriteLine(clientRequestException.ErrorCode);
+                Console.WriteLine(clientRequestException.ErrorMsg);
+                res.Status = EReponseCode.BusinessError;
+                res.Message = clientRequestException.ErrorMsg;
+            }
+            catch (ConnectionException connectionException)
+            {
+                Console.WriteLine(connectionException.ErrorMessage);
+                res.Status = EReponseCode.BusinessError;
+                res.Message = connectionException.ErrorMessage;
+            }
+            return res;
+        }
+    }
+}
---
+++ HuaweiCloudApi/Models/MobileBaseResponseData.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace HuaweiCloudApi.Models
+{
+    /// <summary>
+    /// 基本Response
+    /// </summary>
+    public class MobileBaseResponseData<T>
+    {
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        public EReponseCode Status { get; set; }
+        /// <summary>
+        /// 状态信息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 数据集合
+        /// </summary>
+        public T Data { get; set; }
+    }
+    /// <summary>
+    /// page response data
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PageResponseData<T>
+    {
+        public IList<T> List { get; set; }
+        public long TotalCount { get; set; }
+        public decimal Attribute { get; set; }
+        public decimal Tab1 { get; set; }
+        public decimal Tab2 { get; set; }
+        public bool IsSeller { get; set; }
+    }
+    /// 返回值Code
+    /// </summary>
+    public enum EReponseCode
+    {
+        /// <summary>
+        /// 操作成功
+        /// </summary>
+        [Description("操作成功")]
+        Success = 100,
+        /// <summary>
+        /// 用户未登录
+        /// </summary>
+        [Description("用户未登录")]
+        NoLogin = 102,
+        /// <summary>
+        /// 参数错误
+        /// </summary>
+        [Description("参数错误")]
+        ParameterError = 103,
+        /// <summary>
+        /// 业务错误
+        /// </summary>
+        [Description("业务错误")]
+        BusinessError = 1010,
+        /// <summary>
+        /// 无法获取海报
+        /// </summary>
+        [Description("无法获取海报")]
+        BusinessError_PosterFailed = 101001,
+        /// <summary>
+        /// 加入团失败
+        /// </summary>
+        [Description("加入团失败")]
+        JoinGroupFailed = 101002,
+        /// <summary>
+        /// 系统错误
+        /// </summary>
+        [Description("系统错误")]
+        SysError = 0,
+    }
+}
